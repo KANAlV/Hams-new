97b6fc4aeb7cdce0e439b48775c6a633166268c9
KANAlV <editicroyedics@gmail.com>
2024-12-14 04:15:28 +0800

    linux fix

--- user/drawer.php
+++ user/drawer.php
@@ -20,7 +20,7 @@
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to Log Out?</h3>
-                <a href="/hams-new/user/logout.php" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                <a href="/Hams-new/user/logout.php" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                     Yes, I'm sure
                 </a>
                 <button data-modal-hide="logOut" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
@@ -40,25 +40,25 @@
 
     <?php
         if ($_SESSION['level'] == '4' || $_SESSION['p8'] == '1') {
-            ?><a class="flex items-center h-12" href="/hams-new/user/approval.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/checkbox.png')] md:dark:bg-[url('../resources/dark/checkbox.png')] bg-no-repeat bg-contain">Approval</div></a><?php
+            ?><a class="flex items-center h-12" href="/Hams-new/user/approval.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/checkbox.png')] md:dark:bg-[url('../resources/dark/checkbox.png')] bg-no-repeat bg-contain">Approval</div></a><?php
         }
     ?>
     <?php
         if ($_SESSION['level'] == '4' || $_SESSION['p9'] == '1') {
-            ?><a class="flex items-center h-12" href="/hams-new/user/types.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/category.png')] md:dark:bg-[url('../resources/dark/category.png')] bg-no-repeat bg-contain">Types</div></a><?php
+            ?><a class="flex items-center h-12" href="/Hams-new/user/types.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/category.png')] md:dark:bg-[url('../resources/dark/category.png')] bg-no-repeat bg-contain">Types</div></a><?php
         }
     ?>
-    <a class="flex items-center h-12" href="/hams-new/user/staff.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/staff.png')] md:dark:bg-[url('../resources/dark/staff.png')] bg-no-repeat bg-contain">Staff</div></a>
-    <a class="flex items-center h-12" href="/hams-new/user/medicine.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/medicine.png')] md:dark:bg-[url('../resources/dark/medicine.png')] bg-no-repeat bg-contain">Medicine</div></a>
-    <a class="flex items-center h-12" href="/hams-new/user/equipments.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/equipment.png')] md:dark:bg-[url('../resources/dark/equipment.png')] bg-no-repeat bg-contain">Equipments</div></a>
-    <a class="flex items-center h-12" href="/hams-new/user/rooms.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/room.png')] md:dark:bg-[url('../resources/dark/room.png')] bg-no-repeat bg-contain">Rooms</div></a>
-    <a class="flex items-center h-12" href="/hams-new/user/beds.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/bed.png')] md:dark:bg-[url('../resources/dark/bed.png')] bg-no-repeat bg-contain">Beds</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/staff.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/staff.png')] md:dark:bg-[url('../resources/dark/staff.png')] bg-no-repeat bg-contain">Staff</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/medicine.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/medicine.png')] md:dark:bg-[url('../resources/dark/medicine.png')] bg-no-repeat bg-contain">Medicine</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/equipments.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/equipment.png')] md:dark:bg-[url('../resources/dark/equipment.png')] bg-no-repeat bg-contain">Equipments</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/rooms.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/room.png')] md:dark:bg-[url('../resources/dark/room.png')] bg-no-repeat bg-contain">Rooms</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/beds.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/bed.png')] md:dark:bg-[url('../resources/dark/bed.png')] bg-no-repeat bg-contain">Beds</div></a>
     <?php
         if ($_SESSION['level'] == '4' || $_SESSION['p7'] == '1') {
-            ?><a class="flex items-center h-12" href="/hams-new/user/uid.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/barcode.png')] md:dark:bg-[url('../resources/dark/barcode.png')] bg-no-repeat bg-contain">UID</div></a><?php
+            ?><a class="flex items-center h-12" href="/Hams-new/user/uid.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/barcode.png')] md:dark:bg-[url('../resources/dark/barcode.png')] bg-no-repeat bg-contain">UID</div></a><?php
         }
     ?>
-    <a class="flex items-center h-12" href="/hams-new/user/scanner.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/qr-scan.png')] md:dark:bg-[url('../resources/dark/qr-scan.png')] bg-no-repeat bg-contain">Scanner</div></a>
+    <a class="flex items-center h-12" href="/Hams-new/user/scanner.php"><div class="w-screen px-12 font-bold block md:bg-[url('../resources/qr-scan.png')] md:dark:bg-[url('../resources/dark/qr-scan.png')] bg-no-repeat bg-contain">Scanner</div></a>
     <a class="flex items-center h-4 md:hidden" ><div class="w-screen px-12 font-bold"></div></a>
     <div class="grid grid-cols-2 gap-4">
         <button id="toggleDarkMode" class="px-4 py-2 text-white bg-slate-700 rounded-lg hover:bg-slate-600 focus:ring-4 focus:ring-slate-300 dark:bg-slate-400 dark:hover:bg-slate-300 focus:outline-none dark:focus:ring-slate-500">Dark Mode</button>
